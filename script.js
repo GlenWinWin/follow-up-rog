62cffbc6ee23d86a22c738200233bd9cb8d7e24e
Glenwin Bernabe <glenwinbernabe@gmail.com>
2025-01-10 16:38:42 +0800

    fixed

--- script.js
+++ script.js
@@ -166,8 +166,6 @@ function init() {
                 item[field] = document.getElementById(`modal${field.charAt(0).toUpperCase() + field.slice(1)}`).value;
             });
 
-            state.databases[tab] = item;
-
             // await updateRow(id, [
             //     item.name, 'G', 'G', 'G', 'G', 'G', item.interested, item.discipler, item.texted,
             //     item.update, item.followedUp, item.started
